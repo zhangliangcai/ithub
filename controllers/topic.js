43f068d198e23251fba6156905bdb2b6333652fd
zlc862988337 <862988337@qq.com>
2018-03-26 13:41:57 +0800

    设计路由表&提取多个处理函数模块

---
+++ controllers/topic.js
@@ -0,0 +1,24 @@
+exports.showCreate = (req,res) => {
+    res.send('showCreate')    
+}
+
+exports.create = (req,res) => {
+    res.send('create')    
+}
+
+exports.show = (req,res) => {
+    res.send('show')    
+}
+
+exports.showEdit = (req,res) => {
+    res.send('showEdit')    
+}
+
+exports.edit = (req,res) => {
+    res.send('edit')    
+}
+
+exports.delete = (req,res) => {
+    res.send('delete')    
+}
+
